9c637e9f7d5f1318242dad62ba6b90bef48d2e63
Liliana-MP <liliana.pitra@gmail.com>
2025-04-15 15:54:55 +0200

    wip step functionality

--- src/app/_components/SolarQuoteForm.tsx
+++ src/app/_components/SolarQuoteForm.tsx
@@ -1,16 +1,35 @@
+"use client";
+import { useState } from "react";
 import { EnergyBill } from "./EnergyBill";
 import { LeadForm } from "./LeadForm";
 import { MonthlySaving } from "./MonthlySaving";
 import { RoofSize } from "./RoofSize";
 
+const steps = ["Energy bill", "Roof size", "Monthly savings", "Lead form"];
+
 export const SolarQuoteForm = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const back = () => {
+    if (currentStep > 0) setCurrentStep(currentStep - 1);
+  };
+
+  const next = () => {
+    if (currentStep !== steps.length - 1) setCurrentStep(currentStep + 1);
+  };
+
   return (
     <div>
       <h1>SolarQuoteForm</h1>
-      <EnergyBill />
-      <RoofSize />
-      <MonthlySaving />
-      <LeadForm />
+      <form>
+        <EnergyBill />
+        <RoofSize />
+        <MonthlySaving />
+        <LeadForm />
+      </form>
+      <button disabled={currentStep === 0} onClick={() => back()}>
+        back
+      </button>
+      <button onClick={() => next()}>forw</button>
     </div>
   );
 };
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type QuoteCalc = {
+  energyBill: number;
+  roofSize: "small" | "medium" | "large";
+};
+
+export type Lead = {
+  name: string;
+  email: string;
+  phone: string;
+  postalCode: string;
+};
