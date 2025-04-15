3c95f7562626a21c3a368232bcd6ebd0044df2d6
Liliana-MP <liliana.pitra@gmail.com>
2025-04-15 21:27:29 +0200

    added labels and inputs to LeadForm

--- src/app/_components/LeadForm.tsx
+++ src/app/_components/LeadForm.tsx
@@ -1,3 +1,16 @@
+import styles from "./components.module.css";
+
 export const LeadForm = () => {
-  return <div>LeadForm</div>;
+  return (
+    <div className={styles.container}>
+      <label>Name</label>
+      <input />
+      <label>Email</label>
+      <input />
+      <label>Phone number</label>
+      <input />
+      <label>Postal code</label>
+      <input />
+    </div>
+  );
 };
